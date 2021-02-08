9919d425806e5c0a259704da278028d007093c98
Kenton Lam <kenton_lam@outlook.com>
2021-02-09 01:53:39 +1000

    Basic working version.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # short-link-creator
+
+Very simple single-page pure HTML/JS page which creates short links on a
+short.io custom domain.
+
+No server needed, just host index.html and main.js and you're set.
---
+++ main.js
@@ -0,0 +1,132 @@
+const $ = (...args) => document.querySelector(...args);
+const $$ = (...args) => document.querySelectorAll(...args);
+
+
+/**
+ * @param {Element} el
+ */
+function show(el) {
+  el.classList.remove('hidden');
+}
+
+/**
+ * @param {Element} el
+ */
+function hide(el) {
+  el.classList.add('hidden');
+}
+
+
+/**
+ * @param {HTMLInputElement} el
+ * @param {string} key
+ */
+function persistInput(el, key) {
+  el.value = localStorage.getItem(key) || '';
+
+  el.addEventListener('input', () => {
+    localStorage.setItem(key, el.value.trim());
+  });
+}
+
+
+/**
+ * @param {string} domain
+ * @param {string} short
+ * @param {string} url
+ * @param {string} key
+ * @return {Promise<string>}
+ */
+async function createShortLink(domain, short, url, key) {
+  const data = {
+    domain,
+    path: short,
+    originalURL: url,
+  };
+
+  const resp = await fetch('https://api.short.cm/links/public', {
+    method: 'POST',
+    headers: {
+      'accept': 'application/json',
+      'Content-Type': 'application/json',
+      'authorization': key
+    },
+    body: JSON.stringify(data)
+  });
+
+  if (!resp.ok) {
+    throw `HTTP Error ${resp.status}:\n` + await resp.text();
+  }
+
+  const json = await resp.json();
+  return json['secureShortURL'];
+}
+
+
+// input-related elements
+const domainInput = $('#domain');
+const shortInput = $('#short');
+const urlInput = $('#url');
+const keyInput = $('#key');
+const button = $('#button');
+
+// success elements
+const newUrlLink = $('#newUrl');
+const copyButton = $('#copy');
+const successResult = $('#success');
+
+// failure elements
+const errorMessage = $('#error');
+const failureResult = $('#failure');
+
+// result container element
+const resultContainer = $('#result');
+
+
+// persist api key
+persistInput(keyInput, 'api-key');
+
+// attach submit listener to create link
+$('#form').addEventListener('submit', (ev) => {
+  const [domain, short, url, key] = [
+    domainInput,
+    shortInput,
+    urlInput,
+    keyInput,
+  ].map(el => el.value.trim());
+
+  createShortLink(domain, short, url, key)
+  .then(newUrl => {
+    newUrlLink.textContent = newUrl;
+    newUrlLink.href = newUrl;
+
+    show(successResult);
+    hide(failureResult);
+  })
+  .catch(error => {
+    errorMessage.textContent = error;
+    show(failureResult);
+    hide(successResult);
+  })
+  .finally(() => {
+    show(resultContainer);
+  });
+
+  ev.preventDefault();
+});
+
+// copy handler
+copyButton.addEventListener('click', () => {
+  navigator.clipboard.writeText(newUrlLink.href).then(() => {
+    copyButton.textContent = 'Copied!';
+  });
+})
+
+// extract default domain and short code from URL
+const params = new URLSearchParams(window.location.search);
+domainInput.value = params.get('d') || '';
+shortInput.value = params.get('s') || '';
+
+if (shortInput.value) {
+  urlInput.focus();
+}
